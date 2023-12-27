97687e21c1405fda138342f1cc1c0e6d17f99dab
Nata Nael <natanael280198@gmail.com>
2023-12-27 17:57:13 +0700

    feat: struct example

--- struct.go
+++ struct.go
@@ -1,5 +1,30 @@
 package main
 
+import "log"
+
+type Customer struct {
+	Name, Address string
+	Age int
+}
+
 func main() {
-	
+	var nael Customer
+	nael.Name = "Nata Nael"
+	nael.Age = 25
+	nael.Address = "Jakarta"
+
+	log.Println(nael)
+	log.Println(nael.Name)
+	log.Println(nael.Address)
+	log.Println(nael.Age)
+
+	// evelyn := Customer{
+	// 	Name: "Evelyn Viriya",
+	// 	Address: "Kalideres",
+	// 	Age: 22,
+	// }
+
+	evelyn := Customer{"Evelyn Viriya", "Kalideres", 22}
+
+	log.Println(evelyn)
 }
